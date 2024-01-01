d3aebfab44ed713588ff67bb48e07a556b579b28
yuval2313 <lyuval1210@gmail.com>
2024-01-01 20:05:07 +0200

    Added health check

--- flask-backend/app/__init__.py
+++ flask-backend/app/__init__.py
@@ -38,6 +38,7 @@ def create_app():
 
     with app.app_context():
         from app.routes import error_handlers
+        from app.routes import health
         from app.routes import index
         from app.routes import tasks
 
